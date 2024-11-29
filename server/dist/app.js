05a5d6a243f0db30d0c392eeb789ab82d06fcb0f
Aditya <wpsadi@outlook.com>
2024-11-29 22:24:35 +0530

    deleted dist

--- server/dist/app.js
+++
@@ -1,4 +0,0 @@
-import express from "express";
-const app = express();
-// // consoleole.log('app.ts')
-export default app;
--- server/dist/index.js
+++
@@ -1,6 +0,0 @@
-import app from "./app.js";
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    // consoleole.log(`Server is running on http://localhost:${PORT}`);
-});
-// // consoleole.log('index.ts')
